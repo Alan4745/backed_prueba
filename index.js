3d6ac55bbca4ba6bcfaa14e78fc209655a4c6274
Alan Eduardo De La Cruz Najera <alaneduardodelacruz407@gmail.com>
2022-12-21 14:15:15 -0600

    Cords fixed

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: 'https://re-social.web.app',
-    allowedHeaders: ['my-custom-header'],
     credentials: true,
   },
 });
