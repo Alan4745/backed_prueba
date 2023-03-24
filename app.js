e9ce8375f754d88d83f66b4f2a11deae9cbaac2b
Eduardo-45 <115485379+Alan4745@users.noreply.github.com>
2023-03-24 16:51:21 -0600

    documentacion de app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-/* eslint-disable linebreak-style */
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
+// Importar los paquetes necesarios
+const express = require('express'); // Framework para construir aplicaciones web
+const morgan = require('morgan'); //Middleware que registra los detalles de las solicitudes HTTP
+const cors = require('cors'); // Middleware que ayuda a configurar la politica de mismo origen en la aplicacion
+const fileUpload = require('express-fileupload'); // Middleware que permite gestionar la carga de archivos en el servidor
 
+// Crea la instancia de la aplicacion Express
 const app = express();
 
-// IMPORTACION DE RUTAS
+// ---INICIO---- Importar las rutas que se utilizaran en la aplicacion
 const userRouter = require('./src/routes/user.routes');
 const messageRouter = require('./src/routes/message.routes');
 const conversationRouter = require('./src/routes/Conversation.routes');
@@ -16,32 +17,33 @@ const channelRouter = require('./src/routes/channel.routes');
 const messageChannelRouter = require('./src/routes/messageChannel.routes');
 const tokenRouter = require('./src/routes/token.routes');
 // const postTypeARouter = require('./src/routes/postRoute/postTypeA.routes');
+//---FIN----
 
-// MIDDLEWARES
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
-// CABECERAS
-app.use(cors());
-app.use(morgan('dev'));
+// Configura los middlewares de la aplicacion 
+app.use(express.urlencoded({ extended: false })); // Middleware que analiza los datos de la solicitud HTTP y los pone en un objeto req.body
+app.use(express.json()); // Middleware que analiza los datos de la solicitud HTTP en formato JSON y los pone en un objeto req.body
+app.use(cors()); //Middleware que permiten el acceso a la API desde cualquier origen
+app.use(morgan('dev')); // morgan nos muestra en consola que peticiones estan entrantes a la API
 app.use(
 	fileUpload({
-		useTempFiles: true,
-		tempFileDir: './uploads',
+		useTempFiles: true, // Usa archivos temporales para gestionar la carga de archivos
+		tempFileDir: './uploads', // directorio donde se almacenara los archivos temporales
 	})
 );
 
-// CARGA DE RUTAS localHost:4000/api/productos
+// Configurar las rutas de la aplicacion
 app.use(
-	'/api',
-	userRouter,
-	messageRouter,
-	conversationRouter,
-	postRouter,
-	communityRouter,
-	channelRouter,
-	messageChannelRouter,
-	tokenRouter
+	'/api', // Prefijo que se añadira a todas las rutas
+	userRouter, // rutas para Usuarios
+	messageRouter, // Rutas para mensajes
+	conversationRouter, // Rutas para conversaciones
+	postRouter, //Rutas para publicaciones
+	communityRouter,// Rutas para comunidad
+	channelRouter, // Rutas para Canales
+	messageChannelRouter, // Rutas para mensajes de canal
+	tokenRouter // Rutas para tokens
 );
 
+// Exportamos la instancia de la aplicacion
 module.exports = app;
