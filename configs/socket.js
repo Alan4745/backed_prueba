53afee93fc5ef73512fa14bbe85036332d97499e
Eduardo-45 <115485379+Alan4745@users.noreply.github.com>
2023-03-28 09:36:06 -0600

    documentacion y separcion de codigo

---
+++ configs/socket.js
@@ -0,0 +1,138 @@
+let users = [];
+let rooms = [];
+
+const addUser = (userId, socketId) => {
+	!users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+	users = users.filter((user) => user.socketId !== socketId);
+};
+
+const addUserRoom = (socketId, userName, roomName) => {
+	!rooms.some((room) => room.userName === userName) &&
+    rooms.push({ socketId, userName, roomName });
+};
+
+const removeUserRoom = (socketId) => {
+	rooms = rooms.filter((room) => room.socketId !== socketId);
+};
+
+// eslint-disable-next-line no-unused-vars
+const getUser = (userId) => users.find((user) => user.userId === userId);
+
+const getRoom = (socketId) => rooms.find((room) => room.socketId === socketId);
+
+const socketFunctions = (io) => {
+// iniciamos la conexion del servidor al cliente en tiempo real
+	io.on('connection', (socket) => {
+		console.log('connected to socket.io');
+
+		// -----Inicio----- Al momento de que usuario se conecta al servidor se activa el evento "addUser()" 
+		//socket.on es cuando esta esperando un evnto 
+		//socket.emit es cuando creamos un evento
+		socket.on('addUser', (userId) => {
+		// iniciamos la funcion de agregar el usuario en al array "USERS"
+			addUser(userId, socket.id);
+			//creamos el evento "getUsers"
+			io.emit('getUsers', users);
+			console.log(users);
+		});
+
+		//----FIN----
+
+		// ---Inicio---- al momento de que se conecta a un canal se activa la funcion "addUserRoom()"
+		//socket.on es cuando esta esperando un evnto 
+		//socket.emit es cuando creamos un evento
+		socket.on('joinRoom', ({ userName, roomName }) => {
+		//iniciamos la funcion "addUserRoom" para podre agregar el nombre del canal al array "ROOMS"
+			addUserRoom(socket.id, userName, roomName);
+			//obtenemos los canales para verificar el nombre del canal para poder unirse a la chats
+			const room = getRoom(socket.id);
+			console.log(rooms);
+
+			//socket.join nos permite concetarnos a un socket existente solo pasando el 'nombre del canal' o del 'socket id'
+			socket.join(room.roomName);
+			//socket.broadcast es para trasmiter un evento al canal que esta activo mediante su nombre
+			socket.broadcast
+				.to(room.roomName)
+				.emit('message', `${room.userName} has joined the chat`);
+		});
+		//----FIN----
+
+		//----INICIO---- de la evento "PING PONG"
+		socket.on('ping', () => {
+			socket.emit('pong');
+		});
+		//----FIN----
+
+		//----INICIO---- evento de enviar mensajes del canal
+		socket.on('sendMessageChannel', ({ senderId, text }) => {
+		//verificamos el canales para encotrar el nombre del canal
+			const room = getRoom(socket.id);
+
+			//io.to es a donde va dirigido el mensaje 
+			// retornams los datos del mensaje
+			io.to(room.roomName).emit('messageChannel', {
+				senderId,
+				text,
+				roomName: room.roomName,
+				name: room.userName
+			});
+		});
+		//----FIN----
+
+		//----INICIO---- evento de enviar mensajes Privados
+		socket.on('sendMessage', ({ senderId, recieverId, text }) => {
+		//buscamos en el array "USERS" el socket a quie le vamos a mandar el mensaje
+			const user = getUser(recieverId);
+			//verificamos que si el usuario esta conectado o no
+			if (!user) {
+			// el usuario esta desconetado 
+				console.log('el usuario no esta conectada');
+			} else {
+			//enviamos el mensaje al socket del usuario
+				io.to(user.socketId).emit('getMessage', {
+					senderId,
+					text,
+					recieverId
+				});
+			}
+		});
+		//----FIN----
+
+
+		//---- INICIO ---- evento de desconectar el socket 
+		socket.on('disconnect', () => {
+		// buscamos el socket del canal para poderlos desconectar del servidor
+			const room = getRoom(socket.id);
+			// si es un usuario usara esta funcion para desconetar al uusario del servidor
+			removeUser(socket.id);
+			// verificamos si el canal esta activo aun 
+			if (room) {
+			//emitimos el evento al canal para decir que un usuario salio del chat del canal
+				io.to(room.roomName).emit(
+					'message',
+					`${room.userName} has left the chat`
+				);
+			}
+
+			console.log(`a user Discinnected!${socket.id}`);
+			//aqui removemos el socket del canal si que esta activo
+			removeUserRoom(socket.id);
+			//verificamos si hay aun usuario
+			io.emit('getUsers', users);
+			console.log(users);
+			console.log(rooms);
+
+		//aclaracion aun falta mucho validaciones para ahorrar recursos del servidor
+		//el chat se puede trabajar como un servicio aparte para poder solo el servidor principal de la app libre de cargas inicesarias
+		});
+	});
+
+	//-----------------"Fin" de La funciones del Chat en vivo-----------------------------//
+
+};
+
+module.exports = socketFunctions;
--- src/controllers/channel.controller.js
+++ src/controllers/channel.controller.js
@@ -1,36 +1,51 @@
+// Importar el modelo de datos Channel desde el archivo channelCommunity.model.js
 const Channel = require('../models/channelCommunity.model');
 
+// Definir la función saveChannel que guardará un nuevo canal en la base de datos
 function saveChannel(req, res) {
+	// Crear una nueva instancia de Channel
 	const channelModel = new Channel();
+	// Obtener los parámetros del cuerpo de la solicitud
 	const parameters = req.body;
+	// Obtener el id de la comunidad del parámetro de la URL
 	const { idCommunity } = req.params;
 
+	// Asignar el id de la comunidad, el id del propietario, el nombre del canal y los miembros a la nueva instancia de Channel
 	channelModel.idCommunity = idCommunity;
 	channelModel.idOwner = req.user.sub;
 	channelModel.nameChanel = parameters.nameChanel;
 	channelModel.members = [req.user.sub];
+	
+	// Guardar la nueva instancia de Channel en la base de datos
 	channelModel.save((err, saveChannel) => {
 		if (err) {
+			// Si hay un error, devolver un mensaje de error con el código de estado 500
 			return res.status(500)
 				.send({ message: 'err en la peticion', err });
 		}
 		if (!saveChannel) {
+			// Si no se pudo guardar el canal, devolver un mensaje de error con el código de estado 500
 			return res.status(500)
 				.send({ message: 'err al guardar el Canal' });
 		}
+		// Si se guardó correctamente, devolver un mensaje de éxito con el código de estado 200 y la instancia guardada de Channel
 		return res.status(200).send({ status: 'Success', saveMessage: saveChannel });
 	});
 }
 
+// Definir la función viewChannel que buscará y devolverá todos los canales en los que el usuario actual es miembro
 function viewChannel(req, res) {
+	// Buscar todos los canales en los que el usuario actual es miembro
 	Channel.find(
 		{ members: { $all: [req.user.sub] } },
 		(_err, channelFind) => {
+			// Devolver un objeto con el código de estado 200 y los canales encontrados
 			res.status(200).send({ channel: channelFind });
 		}
 	);
 }
 
+// Exportar las funciones saveChannel y viewChannel para que puedan ser utilizadas en otros archivos
 module.exports = {
 	saveChannel,
 	viewChannel
