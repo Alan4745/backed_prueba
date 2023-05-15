752962a2fb77820cbad6718f72fec9c213f60415
Eduardo-45 <115485379+Alan4745@users.noreply.github.com>
2023-05-15 13:06:22 -0600

    cambios de fromateo de codigo

--- src/controllers/post_test/Post_evento_ticket.js
+++ src/controllers/post_test/Post_evento_ticket.js
@@ -4,23 +4,23 @@ const collectionsModel=require('./../../models/tokens/collections.model');
 const { UploadImg, UploadVideo } = require('../../utils/cloudinary');
 
 async function buscar_evento_ticket(req, res) {
-    evento_tickets.find({ communityId: req.params.id }, (err, find_evento_ticket) => {
-        return res.status(200).send({ message: find_evento_ticket })
-    })
+	evento_tickets.find({ communityId: req.params.id }, (err, find_evento_ticket) => {
+		return res.status(200).send({ message: find_evento_ticket });
+	});
 }
 //BUSCAR EVENTO POR ID DEL EVENTO
 async function buscar_evento_ticket_id(req, res) {
-    evento_tickets.findOne({ _id: req.params.evento_id }, (err, find_evento_ticket_id) => {
-        return res.status(200).send({ message: find_evento_ticket_id })
-    })
+	evento_tickets.findOne({ _id: req.params.evento_id }, (err, find_evento_ticket_id) => {
+		return res.status(200).send({ message: find_evento_ticket_id });
+	});
 }
 
 
 async function editar_evento_ticket(req, res) {
-    console.log(req.params.evento_ticket_id + 'estamos aqui');
-    evento_tickets.findByIdAndUpdate(req.params.evento_ticket_id, {titulo: req.body.titulo, desc: req.body.desc}, {new: true},(err, update_evento_ticket) => {
-        return res.status(200).send({ message: update_evento_ticket })
-    })
+	console.log(req.params.evento_ticket_id + 'estamos aqui');
+	evento_tickets.findByIdAndUpdate(req.params.evento_ticket_id, {titulo: req.body.titulo, desc: req.body.desc}, {new: true},(err, update_evento_ticket) => {
+		return res.status(200).send({ message: update_evento_ticket });
+	});
 }
 
 
@@ -29,65 +29,65 @@ async function editar_evento_ticket(req, res) {
 
 
 async function eliminar_evento_tickets(req, res) {
-    evento_tickets.findByIdAndDelete({ _id: req.params.id_evento }, (err, eliminar_evento_ticket) => {
-        return res.status(200).send({ message: eliminar_evento_ticket })
-    })
+	evento_tickets.findByIdAndDelete({ _id: req.params.id_evento }, (err, eliminar_evento_ticket) => {
+		return res.status(200).send({ message: eliminar_evento_ticket });
+	});
 }
 
 
 //creando evento con tickets 
 async function crear_evento_ticket(req, res){
-evento_ticket_model= new evento_tickets();
-const parameters= req.body;
+	evento_ticket_model= new evento_tickets();
+	const parameters= req.body;
 
 
-evento_ticket_model.titulo = parameters.titulo;
-evento_ticket_model.desc = parameters.descripcion;
-evento_ticket_model.tipoPublicacion = "evento_tickets";
-evento_ticket_model.ubicacion=[{
-    latitud:parameters.latitud, 
-    longitud:parameters.longitud
-}];
-communityModel.findById(parameters.idcom, (_err, comunityfind) => {
-    console.log(comunityfind);
-    //LADO IZQUIERDO ES DE LOS CAMPOS DE PUBLICACION Y LADO DERECHO COMUNIDAD
-    evento_ticket_model.communityId = comunityfind._id
-    evento_ticket_model.communityName = comunityfind.nameCommunity
-})
-collectionsModel.findById(parameters.idticket, (_err, ticketfind) => {
-    console.log(ticketfind);
-    //LADO IZQUIERDO ES DE LOS CAMPOS DE PUBLICACION Y LADO DERECHO COMUNIDAD
-    evento_ticket_model.collectionId = ticketfind._id
-    evento_ticket_model.collectionName = ticketfind.nameCollection
-})
-if (req.files?.image) {
-    const result = await UploadImg(req.files.image.tempFilePath);
-    evento_ticket_model.imagen.public_id = result.public_id;
-    evento_ticket_model.imagen.secure_url = result.secure_url;
-}
+	evento_ticket_model.titulo = parameters.titulo;
+	evento_ticket_model.desc = parameters.descripcion;
+	evento_ticket_model.tipoPublicacion = 'evento_tickets';
+	evento_ticket_model.ubicacion=[{
+		latitud:parameters.latitud, 
+		longitud:parameters.longitud
+	}];
+	communityModel.findById(parameters.idcom, (_err, comunityfind) => {
+		console.log(comunityfind);
+		//LADO IZQUIERDO ES DE LOS CAMPOS DE PUBLICACION Y LADO DERECHO COMUNIDAD
+		evento_ticket_model.communityId = comunityfind._id;
+		evento_ticket_model.communityName = comunityfind.nameCommunity;
+	});
+	collectionsModel.findById(parameters.idticket, (_err, ticketfind) => {
+		console.log(ticketfind);
+		//LADO IZQUIERDO ES DE LOS CAMPOS DE PUBLICACION Y LADO DERECHO COMUNIDAD
+		evento_ticket_model.collectionId = ticketfind._id;
+		evento_ticket_model.collectionName = ticketfind.nameCollection;
+	});
+	if (req.files?.image) {
+		const result = await UploadImg(req.files.image.tempFilePath);
+		evento_ticket_model.imagen.public_id = result.public_id;
+		evento_ticket_model.imagen.secure_url = result.secure_url;
+	}
 
 
-if (req.files?.video) {
-    const result = await UploadVideo(req.files.image.tempFilePath);
-    evento_ticket_model.video.public_id = result.public_id;
-    evento_ticket_model.video.secure_url = result.secure_url;
-}
+	if (req.files?.video) {
+		const result = await UploadVideo(req.files.image.tempFilePath);
+		evento_ticket_model.video.public_id = result.public_id;
+		evento_ticket_model.video.secure_url = result.secure_url;
+	}
 
 
-evento_ticket_model.save((err, event_ticket) => {
-    if (err) {
-        return res.status(500).send({ message: 'err en la peticion' });
-    }
+	evento_ticket_model.save((err, event_ticket) => {
+		if (err) {
+			return res.status(500).send({ message: 'err en la peticion' });
+		}
 
-    // si la repuesta de la base de datos nos regresa "null"
-    if (!event_ticket) {
-        return res
-            .status(500)
-            .send({ message: 'err al guardar el usuario' });
-    }
+		// si la repuesta de la base de datos nos regresa "null"
+		if (!event_ticket) {
+			return res
+				.status(500)
+				.send({ message: 'err al guardar el usuario' });
+		}
 
-    return res.status(200).send({ message: event_ticket })
-})
+		return res.status(200).send({ message: event_ticket });
+	});
 }
 
 
@@ -95,9 +95,9 @@ evento_ticket_model.save((err, event_ticket) => {
 
 //exportar los modulos
 module.exports = {
-   crear_evento_ticket,
-   eliminar_evento_tickets,
-   buscar_evento_ticket,
-   buscar_evento_ticket_id,
-   editar_evento_ticket
+	crear_evento_ticket,
+	eliminar_evento_tickets,
+	buscar_evento_ticket,
+	buscar_evento_ticket_id,
+	editar_evento_ticket
 };
