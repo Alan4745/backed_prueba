fa027f21d6465ba0e06c8deb04f6d4a360a410c9
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2023-11-27 11:36:20 -0600

    cambios de funcio de try catch
    esta cambiando las funciones a mejor legibilidad y documentacion del proyecto

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -6,194 +6,307 @@ const fs = require('fs-extra');
 const saltRounds = 10;
 
 // funcion para poder registra nuevos usuario en la base de datos
+// async function userRegistration(req, res) {
+// 	// creamos un nuevo objeto de "User"
+// 	const userModel = new User();
+// 	// rastreamos los datos que viene en la petecion
+// 	const parameters = req.body;
+
+// 	// console.log(parameters.name, parameters.nickName, parameters.email, parameters.password);
+
+// 	// Verificamos que los parametros no venga vacios
+// 	if (
+// 		!parameters.name ||
+//     !parameters.nickName ||
+//     !parameters.email ||
+//     !parameters.password
+// 	) {
+// 		return res
+// 			.status(400)
+// 			.send({ message: `Required data: name:${parameters.name}, nickName: ${parameters.nickName}, email: ${parameters.email}, password: ${parameters.email}` });
+// 	}
+
+// 	// la funcion "find" va a buscar en la base de datos,
+// 	// donde coincidan el parametro "email"
+// 	User.find({ email: parameters.email }, (_err, userFind) => {
+// 		// Verificamos si traer parametro "email" de la peticion
+// 		if (userFind.length > 0) {
+// 			return res.status(500).send({ message: 'This email is already used' });
+// 		}
+// 		// verificamos si traer el parametro "nickName" de la peticion
+// 		User.find(
+// 			{ nickName: `@${parameters.nickName}` },
+// 			async (_err, nickNameFind) => {
+// 				if (nickNameFind.length > 0) {
+// 					return res
+// 						.status(500)
+// 						.send({ message: 'This NickName is already used' });
+// 				}
+
+// 				// aqui verificamos si nuestra peticion traer un archivo o imagen
+// 				if (req.files?.image) {
+// 					try {
+// 						// función para subir las imagen a cloudinary
+// 						console.log(req.files.image.tempFilePath);
+// 						const result = await UploadImg(req.files.image.tempFilePath);
+// 						console.log(result);
+
+// 						// Guardamos los datos que nos devuelve cloudinary
+// 						userModel.imageAvatar.public_id = result.public_id;
+// 						userModel.imageAvatar.secure_url = result.secure_url;
+
+// 						await fs.unlink(req.files.image.tempFilePath);
+// 					} catch (error) {
+// 						await fs.unlink(req.files.image.tempFilePath);
+
+// 						// captura y manejo de la excepción
+// 						console.error('Se produjo un error:', error);
+// 						return res.status(500).send({ message: error });
+// 					}
+// 				}
+
+// 				// ---guardamos los parametro en el objeto "userModel"---
+// 				userModel.name = parameters.name;
+// 				userModel.nickName = `@${parameters.nickName}`;
+// 				userModel.email = parameters.email;
+// 				// ---
+
+// 				// "bcrypt" donde se encarga de incritar la contraseña,
+// 				// antes de que mande a la base de datos
+// 				bcrypt.hash(parameters.password, saltRounds, (_err, hash) => {
+// 					userModel.password = hash;
+
+// 					// guardamos el objeto "usermodel" para ahora subirlo a la base de datos
+// 					userModel.save((err, userSave) => {
+// 						// aqui capturamos que la base de datos nos devuelva
+// 						if (err) {
+// 							return res.status(500).send({ message: 'err en la peticion' });
+// 						}
+
+// 						// si la repuesta de la base de datos nos regresa "null"
+// 						if (!userSave) {
+// 							return res
+// 								.status(500)
+// 								.send({ message: 'err al guardar el usuario' });
+// 						}
+
+// 						// si todo salio bien, aqui mostraremos en la respuesta
+// 						return res.status(200).send({ UserInfo: userSave });
+// 					});
+// 				});
+// 			}
+// 		);
+// 	});
+// }
+
 async function userRegistration(req, res) {
-	// creamos un nuevo objeto de "User"
-	const userModel = new User();
-	// rastreamos los datos que viene en la petecion
-	const parameters = req.body;
+	try {
+		// Crear una nueva instancia del modelo de usuario
+		const userModel = new User();
+		// Obtener los parámetros de la solicitud
+		const parameters = req.body;
 
-	// console.log(parameters.name, parameters.nickName, parameters.email, parameters.password);
-
-	// Verificamos que los parametros no venga vacios
-	if (
-		!parameters.name ||
-		!parameters.nickName ||
-		!parameters.email ||
-		!parameters.password
-	) {
-		return res.status(400).send({ message: 'Required data' });
-	}
-
-	// la funcion "find" va a buscar en la base de datos,
-	// donde coincidan el parametro "email"
-	User.find({ email: parameters.email }, (_err, userFind) => {
-		// Verificamos si traer parametro "email" de la peticion
-		if (userFind.length > 0) {
-			return res.status(500).send({ message: 'This email is already used' });
+		// Verificar que los datos requeridos estén presentes
+		if (!parameters.name ||
+        !parameters.nickName ||
+        !parameters.email ||
+        !parameters.password
+		) {
+			return res.status(400).send({ message: 'Required data is missing.' });
 		}
-		// verificamos si traer el parametro "nickName" de la peticion
-		User.find(
-			{ nickName: `@${parameters.nickName}` },
-			async (_err, nickNameFind) => {
-				if (nickNameFind.length > 0) {
-					return res
-						.status(500)
-						.send({ message: 'This NickName is already used' });
-				}
 
-				// aqui verificamos si nuestra peticion traer un archivo o imagen
-				if (req.files?.image) {
-					try {
-						// función para subir las imagen a cloudinary
-						console.log(req.files.image.tempFilePath);
-						const result = await UploadImg(req.files.image.tempFilePath);
-						console.log(result);
+		// Buscar si ya existe un usuario con el mismo correo electrónico
+		const userByEmail = await User.find({ email: parameters.email });
+		if (userByEmail.length > 0) {
+			return res.status(400).send({ message: 'This email is already in use.' });
+		}
 
-						// Guardamos los datos que nos devuelve cloudinary
-						userModel.imageAvatar.public_id = result.public_id;
-						userModel.imageAvatar.secure_url = result.secure_url;
+		// Buscar si ya existe un usuario con el mismo apodo (nickName)
+		const userByNickName = await User.find({
+			nickName: `@${parameters.nickName}`,
+		});
+		if (userByNickName.length > 0) {
+			return res
+				.status(400)
+				.send({ message: 'This NickName is already in use.' });
+		}
 
-						await fs.unlink(req.files.image.tempFilePath);
-					} catch (error) {
-						await fs.unlink(req.files.image.tempFilePath);
+		// Si se adjunta una imagen en la solicitud
+		if (req.files?.image) {
+			// Subir la imagen a Cloudinary y obtener el resultado
+			const result = await UploadImg(req.files.image.tempFilePath);
+			// Guardar la información de la imagen en el modelo de usuario
+			userModel.imageAvatar.public_id = result.public_id;
+			userModel.imageAvatar.secure_url = result.secure_url;
 
-						// captura y manejo de la excepción
-						console.error('Se produjo un error:', error);
-						return res
-							.status(500)
-							.send({ message: error });
-					}
-				}
-
-				// ---guardamos los parametro en el objeto "userModel"---
-				userModel.name = parameters.name;
-				userModel.nickName = `@${parameters.nickName}`;
-				userModel.email = parameters.email;
-				// ---
-
-				// "bcrypt" donde se encarga de incritar la contraseña,
-				// antes de que mande a la base de datos
-				bcrypt.hash(parameters.password, saltRounds, (_err, hash) => {
-					userModel.password = hash;
-
-					// guardamos el objeto "usermodel" para ahora subirlo a la base de datos
-					userModel.save((err, userSave) => {
-						// aqui capturamos que la base de datos nos devuelva
-						if (err) {
-							return res.status(500).send({ message: 'err en la peticion' });
-						}
-
-						// si la repuesta de la base de datos nos regresa "null"
-						if (!userSave) {
-							return res
-								.status(500)
-								.send({ message: 'err al guardar el usuario' });
-						}
-
-						// si todo salio bien, aqui mostraremos en la respuesta
-						return res.status(200).send({ UserInfo: userSave });
-					});
-				});
+			// Verificar si el archivo temporal existe antes de intentar eliminarlo
+			if (fs.existsSync(req.files.image.tempFilePath)) {
+				await fs.unlink(req.files.image.tempFilePath);
+			} else {
+				console.warn('El archivo temporal no existe.');
 			}
-		);
-	});
+		}
+
+		// Asignar los demás datos al modelo de usuario
+		userModel.name = parameters.name;
+		userModel.nickName = `@${parameters.nickName}`;
+		userModel.email = parameters.email;
+		// Hashear la contraseña antes de almacenarla en la base de datos
+		const hash = await bcrypt.hash(parameters.password, saltRounds);
+		userModel.password = hash;
+		// Guardar el modelo de usuario en la base de datos
+		const userSave = await userModel.save();
+		// Verificar si la operación de guardado fue exitosa
+		if (!userSave) {
+			return res.status(500).send({ message: 'Error saving the user.' });
+		}
+
+		// Enviar una respuesta exitosa con la información del usuario guardado
+		return res.status(200).send({ UserInfo: userSave });
+	} catch (error) {
+		// Capturar y manejar cualquier error que ocurra durante el procesoconsole.error('An error occurred:', error);
+		return res.status(500).send({ message: 'Internal server error.' });
+	}
 }
 
 // funcion de inicio de sesion de parte del servidor
-function loginUser(req, res) {
-	// rastreamos los datos que viene en la petecion
-	const parameters = req.body;
-	// expresion regular para poder aceptar los "nickNames" de los usuarios
-	const patron = /^@/;
+// function loginUser(req, res) {
+// 	// rastreamos los datos que viene en la petecion
+// 	const parameters = req.body;
+// 	// expresion regular para poder aceptar los "nickNames" de los usuarios
+// 	const patron = /^@/;
 
-	// Verificamos que los parametros no vengan vacios
-	if (!parameters.crede || !parameters.password) {
-		return res.status(500).send({ message: 'Falta datos' });
-	}
+// 	// Verificamos que los parametros no vengan vacios
+// 	if (!parameters.crede || !parameters.password) {
+// 		return res.status(500).send({ message: 'Falta datos' });
+// 	}
 
-	// Aqui Verificamos que nuestro "espresion regular si se cumple o no"
-	if (patron.test(parameters.crede)) {
-		// mandamos a buscar la informacion del usuario a la base de datos
-		User.findOne({ nickName: parameters.crede }, (err, userFind) => {
-			// aqui capturamos que la base de datos nos devuelva
-			if (err) return res.status(500).send({ message: 'Error en la peticion' });
-			// si la repuesta de la base de datos nos regresa "null"
-			if (!userFind) return res.status(500).send({ message: 'user no find' });
-			// verificamos si el objeto "userFind" vien con algun objeto
-			if (userFind) {
-				// verificamos las contraseñas la que viene de la peticion
-				// y la que esta en la base de datos
-				bcrypt.compare(
-					parameters.password,
-					userFind.password,
-					(_err, verifiedPassword) => {
-						// volvemos a solicitar los datos a la base de datos para comprobar
-						// que nuestra valicion de las contraseñas sea "true"
-						User.findOne(
-							{ nickName: parameters.crede },
-							{ password: 0 },
-							(_err, userFindIdentidad) => {
-								// verificamos los que nos devolvido "bcrypt" aya sido "true"
-								if (verifiedPassword) {
-									return res
-										.status(200)
-										.send({
-											token: jwt.crearToken(userFind),
-											user: userFindIdentidad,
-										});
-								}
-								// el caso que "brcrypt" nos aya devolvido "false"
-								return res
-									.status(500)
-									.send({ mensaje: 'email or password does not match' });
-							}
-						);
-					}
-				);
-			}
-		});
-	} else {
-		// este el caso que alla ingresado un correo como metodo de inicio de sesion
-		User.findOne({ email: parameters.crede }, (err, userFind) => {
-			// aqui capturamos que la base de datos nos devuelva
-			if (err) return res.status(500).send({ message: 'Error en la peticion' });
-			// si la repuesta de la base de datos nos regresa "null"
-			if (!userFind) return res.status(500).send({ message: 'user no find' });
-			// verificamos si el objeto "userFind" vien con algun objeto
-			if (userFind) {
-				// verificamos las contraseñas la que viene de la peticion
-				// y la que esta en la base de datos
-				bcrypt.compare(
-					parameters.password,
-					userFind.password,
-					(_err, verifiedPassword) => {
-						// volvemos a solicitar los datos a la base de datos para comprobar
-						// que nuestra valicion de las contraseñas sea "true"
-						User.findOne(
-							{ email: parameters.crede },
-							{ password: 0 },
-							(_err, userFindIdentidad) => {
-								// verificamos los que nos devolvido "bcrypt" aya sido "true"
-								if (verifiedPassword) {
-									return res
-										.status(200)
-										.send({
-											token: jwt.crearToken(userFind),
-											user: userFindIdentidad,
-										});
-								}
-								// el caso que "brcrypt" nos aya devolvido "false"
-								return res
-									.status(500)
-									.send({ mensaje: 'email or password does not match' });
-							}
-						);
-					}
-				);
-			}
-		});
+// 	// Aqui Verificamos que nuestro "espresion regular si se cumple o no"
+// 	if (patron.test(parameters.crede)) {
+// 		// mandamos a buscar la informacion del usuario a la base de datos
+// 		User.findOne({ nickName: parameters.crede }, (err, userFind) => {
+// 			// aqui capturamos que la base de datos nos devuelva
+// 			if (err) return res.status(500).send({ message: 'Error en la peticion' });
+// 			// si la repuesta de la base de datos nos regresa "null"
+// 			if (!userFind) return res.status(500).send({ message: 'user no find' });
+// 			// verificamos si el objeto "userFind" vien con algun objeto
+// 			if (userFind) {
+// 				// verificamos las contraseñas la que viene de la peticion
+// 				// y la que esta en la base de datos
+// 				bcrypt.compare(
+// 					parameters.password,
+// 					userFind.password,
+// 					(_err, verifiedPassword) => {
+// 						// volvemos a solicitar los datos a la base de datos para comprobar
+// 						// que nuestra valicion de las contraseñas sea "true"
+// 						User.findOne(
+// 							{ nickName: parameters.crede },
+// 							{ password: 0 },
+// 							(_err, userFindIdentidad) => {
+// 								// verificamos los que nos devolvido "bcrypt" aya sido "true"
+// 								if (verifiedPassword) {
+// 									return res.status(200).send({
+// 										token: jwt.crearToken(userFind),
+// 										user: userFindIdentidad,
+// 									});
+// 								}
+// 								// el caso que "brcrypt" nos aya devolvido "false"
+// 								return res
+// 									.status(500)
+// 									.send({ mensaje: 'email or password does not match' });
+// 							}
+// 						);
+// 					}
+// 				);
+// 			}
+// 		});
+// 	} else {
+// 		// este el caso que alla ingresado un correo como metodo de inicio de sesion
+// 		User.findOne({ email: parameters.crede }, (err, userFind) => {
+// 			// aqui capturamos que la base de datos nos devuelva
+// 			if (err) return res.status(500).send({ message: 'Error en la peticion' });
+// 			// si la repuesta de la base de datos nos regresa "null"
+// 			if (!userFind) return res.status(500).send({ message: 'user no find' });
+// 			// verificamos si el objeto "userFind" vien con algun objeto
+// 			if (userFind) {
+// 				// verificamos las contraseñas la que viene de la peticion
+// 				// y la que esta en la base de datos
+// 				bcrypt.compare(
+// 					parameters.password,
+// 					userFind.password,
+// 					(_err, verifiedPassword) => {
+// 						// volvemos a solicitar los datos a la base de datos para comprobar
+// 						// que nuestra valicion de las contraseñas sea "true"
+// 						User.findOne(
+// 							{ email: parameters.crede },
+// 							{ password: 0 },
+// 							(_err, userFindIdentidad) => {
+// 								// verificamos los que nos devolvido "bcrypt" aya sido "true"
+// 								if (verifiedPassword) {
+// 									return res.status(200).send({
+// 										token: jwt.crearToken(userFind),
+// 										user: userFindIdentidad,
+// 									});
+// 								}
+// 								// el caso que "brcrypt" nos aya devolvido "false"
+// 								return res
+// 									.status(500)
+// 									.send({ mensaje: 'email or password does not match' });
+// 							}
+// 						);
+// 					}
+// 				);
+// 			}
+// 		});
+// 	}
+// }
+
+async function loginUser(req, res) {
+	try {
+		// Obtener los datos de la solicitud
+		const parameters = req.body;
+		// Expresión regular para aceptar "nickNames" de usuarios
+		const patron = /^@/;
+  
+		// Verificar que los parámetros no estén vacíos
+		if (!parameters.crede || !parameters.password) {
+			return res.status(400).send({ message: 'Missing data in the request.' });
+		}
+  
+		let userFind;
+  
+		// Verificar si el parámetro "crede" coincide con el patrón de "nickName"
+		if (patron.test(parameters.crede)) {
+		// Buscar la información del usuario en la base de datos por "nickName"
+			userFind = await User.findOne({ nickName: parameters.crede });
+		} else {
+		// En caso de que el "crede" no sea un "nickName" (puede ser un correo electrónico)
+			userFind = await User.findOne({ email: parameters.crede });
+		}
+  
+		// Si el usuario no se encuentra
+		if (!userFind) {
+			return res.status(404).send({ message: 'User not found.' });
+		}
+  
+		// Verificar las contraseñas
+		const verifiedPassword = await bcrypt.compare(parameters.password, userFind.password);
+  
+		// Si la validación de las contraseñas fue exitosa
+		if (verifiedPassword) {
+			const token = jwt.crearToken(userFind);
+			return res.status(200).send({ token, user: userFind });
+		}
+  
+		// En caso de que la validación de las contraseñas falle
+		return res.status(401).send({ message: 'Email or password does not match.' });
+	} catch (error) {
+		console.error('An error occurred:', error);
+		return res.status(500).send({ message: 'Internal server error.' });
 	}
 }
+  
+  
 
 module.exports = {
 	userRegistration,
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,41 +1,99 @@
 const User = require('../models/user.model');
 
-function updateUser(req, res) {
-	const { idUser } = req.params.idUser;
-	const parameters = req.body;
+// function updateUser(req, res) {
+// 	const { idUser } = req.params.idUser;
+// 	const parameters = req.body;
 
-	// Eliminadadon la entrada de de los siguientes parametros
-	delete parameters.password;
-	delete parameters.rol;
-	delete parameters.email;
+// 	// Eliminadadon la entrada de de los siguientes parametros
+// 	delete parameters.password;
+// 	delete parameters.rol;
+// 	delete parameters.email;
 
-	// verificamos que si el usuario le pertenece el perfil
-	if (req.user.sub !== idUser) {
-		return res.status(500).send({ mensaje: 'No tiene los permisos para editar este Usuario.' });
+// 	// verificamos que si el usuario le pertenece el perfil
+// 	if (req.user.sub !== idUser) {
+// 		return res.status(500).send({ mensaje: 'No tiene los permisos para editar este Usuario.' });
+// 	}
+
+// 	User.findByIdAndUpdate(idUser, parameters, { new: true }, (err, userUpdate) => {
+// 		if (err) return res.status(500).send({ message: 'Erro en la pericion' });
+// 		if (!userUpdate) return res.status(500).send({ message: 'error Al Editar el usuario' });
+
+// 		return res.status(200).send({ UserInfo: userUpdate });
+// 	});
+// }
+
+async function updateUser(req, res) {
+	try {
+		const { idUser } = req.params;
+		const parameters = req.body;
+  
+		// Eliminando la entrada de los siguientes parámetros
+		delete parameters.password;
+		delete parameters.rol;
+		delete parameters.email;
+  
+		// Verificamos si el usuario le pertenece al perfil
+		if (req.user.sub !== idUser) {
+			return res.status(403).send({ mensaje: 'No tiene los permisos para editar este usuario.' });
+		}
+  
+		// Actualizar el usuario y obtener el resultado actualizado
+		const userUpdate = await User.findByIdAndUpdate(idUser, parameters, { new: true });
+  
+		// Verificar si la actualización fue exitosa
+		if (!userUpdate) {
+			return res.status(500).send({ message: 'Error al editar el usuario.' });
+		}
+  
+		// Enviar la información actualizada del usuario
+		return res.status(200).send({ message: userUpdate });
+	} catch (error) {
+		console.error('An error occurred:', error);
+		return res.status(500).send({ message: 'Internal server error.' });
 	}
-
-	User.findByIdAndUpdate(idUser, parameters, { new: true }, (err, userUpdate) => {
-		if (err) return res.status(500).send({ message: 'Erro en la pericion' });
-		if (!userUpdate) return res.status(500).send({ message: 'error Al Editar el usuario' });
-
-		return res.status(200).send({ UserInfo: userUpdate });
-	});
 }
+  
 
-function deleteUser(req, res) {
-	const { idUser } = req.params.idUser;
+// function deleteUser(req, res) {
+// 	const { idUser } = req.params.idUser;
 
-	if (req.user.sub !== idUser) {
-		return res.status(500).send({ mensaje: 'No tiene los permisos para eliminar este Usuario.' });
+// 	if (req.user.sub !== idUser) {
+// 		return res.status(500).send({ mensaje: 'No tiene los permisos para eliminar este Usuario.' });
+// 	}
+
+// 	User.findByIdAndDelete(idUser, (err, userDelete) => {
+// 		if (err) return res.status(500).send({ message: 'error en la peticion' });
+// 		if (!userDelete) return res.status(500).send({ message: 'erro en la peticion' });
+
+// 		return res.status(200).send({ UserInfo: userDelete });
+// 	});
+// }
+
+async function deleteUser(req, res) {
+	try {
+		const { idUser } = req.params;
+  
+		// Verificar si el usuario tiene los permisos para eliminar este usuario
+		if (req.user.sub !== idUser) {
+			return res.status(403).send({ mensaje: 'No tiene los permisos para eliminar este usuario.' });
+		}
+  
+		// Eliminar el usuario y obtener el resultado eliminado
+		const userDelete = await User.findByIdAndDelete(idUser);
+  
+		// Verificar si la eliminación fue exitosa
+		if (!userDelete) {
+			return res.status(500).send({ message: 'Error al eliminar el usuario.' });
+		}
+  
+		// Enviar la información del usuario eliminado
+		return res.status(200).send({ message: userDelete });
+	} catch (error) {
+		console.error('An error occurred:', error);
+		return res.status(500).send({ message: 'Internal server error.' });
 	}
-
-	User.findByIdAndDelete(idUser, (err, userDelete) => {
-		if (err) return res.status(500).send({ message: 'error en la peticion' });
-		if (!userDelete) return res.status(500).send({ message: 'erro en la peticion' });
-
-		return res.status(200).send({ UserInfo: userDelete });
-	});
 }
+  
 
 function viewUser(req, res) {
 	User.find((err, usersView) => res.status(200).send({ UserInfo: usersView }));
