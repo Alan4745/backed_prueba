a2ce4ea2e7d9242b4ba302cfd42bbee0b20e0e33
Luis Galea <luisgalea31@gmail.com>
2024-07-23 17:27:22 -0500

    feat<src/controllers/points/points.controller.js>: add news funcs and add polygon

--- src/controllers/points/points.controller.js
+++ src/controllers/points/points.controller.js
@@ -1,20 +1,28 @@
-const {Points} = require('../../models/points/points.model')
+const { Points } = require('../../models/points/points.model');
 
-const createPerimeterPoints = async(req, res) => {
-    const { amount, longitude, latitude, radius, emitterId } = req.body;
+const createPerimeterPoints = async (req, res) => {
+    const { amount, type, coordinates, radius, emitterId } = req.body;
 
-    if (!amount || !longitude || !latitude || !radius || !emitterId) {
+    if (!amount || !type || !coordinates || !emitterId) {
         return res.status(400).json({ message: 'Faltan datos requeridos' });
+    }
+
+    if (type === 'Point' && (!Array.isArray(coordinates) || coordinates.length !== 2)) {
+        return res.status(400).json({ message: 'Coordenadas inválidas para Point' });
+    }
+
+    if (type === 'Polygon' && (!Array.isArray(coordinates) || coordinates.length < 4 || !coordinates.every(coord => Array.isArray(coord) && coord.length === 2))) {
+        return res.status(400).json({ message: 'Coordenadas inválidas para Polygon' });
     }
 
     try {
         const newPoint = new Points({
             amount,
             location: {
-                type: 'Point',
-                coordinates: [longitude, latitude]
+                type,
+                coordinates: type === 'Polygon' ? [coordinates] : coordinates
             },
-            radius,
+            radius: type === 'Point' ? radius : undefined,
             emitter: emitterId
         });
 
@@ -24,8 +32,19 @@ const createPerimeterPoints = async(req, res) => {
         console.error('Error al crear el punto:', error);
         res.status(500).json({ message: 'Error al crear el punto', error });
     }
-}
+};
+
+const getPerimeterPoints = async (req, res) => {
+    try {
+        const perimeterPoints = await Points.find();
+        res.status(200).json(perimeterPoints);
+    } catch (error) {
+        console.error('Error al consultar el punto:', error);
+        res.status(500).json({ message: 'Error al consultar el punto:', error });
+    }
+};
 
 module.exports = {
-    createPerimeterPoints
-}
+    createPerimeterPoints,
+    getPerimeterPoints
+};
