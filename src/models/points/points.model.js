97a351bc1bc093e842ed213814e062e664499ce3
Luis Galea <luisgalea31@gmail.com>
2024-07-23 17:29:13 -0500

    feat<src/models/points/points.model.js>: add funcs to accept polygons

--- src/models/points/points.model.js
+++ src/models/points/points.model.js
@@ -8,18 +8,35 @@ const pointsSchema = new Schema({
     },
     location: {
         type: {
-            type: String, // 'Point'
-            enum: ['Point'],
+            type: String, // 'Point' or 'Polygon'
+            enum: ['Point', 'Polygon'],
             required: true
         },
         coordinates: {
-            type: [Number], // [longitude, latitude]
-            required: true
+            type: Array, // Can be [longitude, latitude] for Point or an array of arrays for Polygon
+            required: true,
+            validate: {
+                validator: function(value) {
+                    if (this.location.type === 'Point') {
+                        return value.length === 2 && typeof value[0] === 'number' && typeof value[1] === 'number';
+                    } else if (this.location.type === 'Polygon') {
+                        return value.length > 0 && value.every(ring =>
+                            Array.isArray(ring) && ring.length > 3 && ring[0].length === 2 &&
+                            ring.every(coord => Array.isArray(coord) && coord.length === 2 && typeof coord[0] === 'number' && typeof coord[1] === 'number') &&
+                            ring[0][0] === ring[ring.length - 1][0] && ring[0][1] === ring[ring.length - 1][1]
+                        );
+                    }
+                    return false;
+                },
+                message: props => `${props.value} no es una coordenada válida para ${props.type}`
+            }
         }
     },
     radius: {
         type: Number, // en metros
-        required: true
+        required: function() {
+            return this.location.type === 'Point';
+        }
     },
     emitter: {
         type: Schema.Types.ObjectId, // referencia al usuario emisor
