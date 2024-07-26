e80789d7c6399a813272cbdc931c38a0b5b808c5
Luis Galea <luisgalea31@gmail.com>
2024-07-26 09:34:54 -0500

    feat<src/routes/points/points.routes.js>: add new routes redeemed and marked

--- src/routes/points/points.routes.js
+++ src/routes/points/points.routes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const { createPerimeterPoints, getPerimeterPoints } = require("../../controllers/points/points.controller");
+const { createPointsRedeemed, getPointsRedeemed } = require("../../controllers/points/pointsReemed.controller");
+const { createPointsMarked, getAllPointsMarked } = require("../../controllers/points/pointsMarked.controller");
 const api = express.Router();
 
+// Rutas para emitir puntos
 api.get('/getPerimeterPoints', getPerimeterPoints)
 api.post('/createPerimeterPoints', createPerimeterPoints)
+
+// Rutas para marcar puntos
+api.get('/getMarkedPoints', getAllPointsMarked)
+api.post('/createMarkedPoints', createPointsMarked)
+
+// Rutas para canjear puntos
+api.get('/getPointsRedeemed', getPointsRedeemed)
+api.post('/createPointsRedeemed', createPointsRedeemed)
 
 module.exports = api
