cf3a7d17426efa3321f7f8165b8971141957589f
Luis Galea <luisgalea31@gmail.com>
2024-07-29 16:36:54 -0500

    feat<src/routes/points/points.routes.js>: add news routes

--- src/routes/points/points.routes.js
+++ src/routes/points/points.routes.js
@@ -1,19 +1,36 @@
 const express = require("express");
-const { createPerimeterPoints, getPerimeterPoints } = require("../../controllers/points/points.controller");
-const { createPointsRedeemed, getPointsRedeemed } = require("../../controllers/points/pointsReemed.controller");
-const { createPointsMarked, getAllPointsMarked } = require("../../controllers/points/pointsMarked.controller");
+const { createPerimeterPoints, 
+        getPerimeterPoints, 
+        getPerimeterPointById, 
+        updatePerimeterPointById, 
+        deletePerimeterPointById 
+    } = require("../../controllers/points/points.controller");
+const { createPointsRedeemed, getPointsRedeemed, getPointsRedeemedById, deletePointRedeemedById } = require("../../controllers/points/pointsReemed.controller");
+const { createPointsMarked, 
+        getAllPointsMarked, 
+        getPointMarkedById, 
+        updatePointMarkedById, 
+        deletePointMarkedById } = require("../../controllers/points/pointsMarked.controller");
 const api = express.Router();
 
 // Rutas para emitir puntos
 api.get('/getPerimeterPoints', getPerimeterPoints)
+api.get('/getPerimeterPoints/:id', getPerimeterPointById)
 api.post('/createPerimeterPoints', createPerimeterPoints)
+api.put('/updatePerimeterPointsById/:id', updatePerimeterPointById)
+api.delete('/deletePerimeterPointById/:id', deletePerimeterPointById)
 
 // Rutas para marcar puntos
 api.get('/getMarkedPoints', getAllPointsMarked)
+api.get('/getMarkedPoints/:id', getPointMarkedById)
 api.post('/createMarkedPoints', createPointsMarked)
+api.put('/updatePointsMarkedById/:id', updatePointMarkedById)
+api.delete('/deletePointsMarkedById/:id', deletePointMarkedById)
 
 // Rutas para canjear puntos
 api.get('/getPointsRedeemed', getPointsRedeemed)
+api.get('/getPointsRedeemed/:id', getPointsRedeemedById)
 api.post('/createPointsRedeemed', createPointsRedeemed)
+api.delete('/deletePointsRedeemedById/:id', deletePointRedeemedById)
 
 module.exports = api
