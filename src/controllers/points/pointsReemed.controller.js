7cc5048c2306054b60df0dd7111950d7b521dc77
Luis Galea <luisgalea31@gmail.com>
2024-07-29 16:41:01 -0500

    feat<src/controllers/points/pointsRedeemed.controller.js>: add new features and fix them

--- src/controllers/points/pointsReemed.controller.js
+++ src/controllers/points/pointsReemed.controller.js
@@ -6,6 +6,16 @@ const createPointsRedeemed = async (req, res) => {
     const { type, coordinates, idPointsMarked, receiver } = req.body;
 
     try {
+        // Asegurarse de que las coordenadas están en el formato correcto [latitud, longitud]
+        if (!Array.isArray(coordinates) || coordinates.length !== 2) {
+            return res.status(400).json({ message: 'Coordenadas inválidas' });
+        }
+
+        // Verificar que la latitud y la longitud están en los rangos correctos
+        const [latitude, longitude] = coordinates;
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180) {
+            return res.status(400).json({ message: 'Coordenadas fuera de los límites permitidos' });
+        }
         // Buscar los puntos al canjear por Id, y verificar que exista
         const pointsFounds = await PointsMarked.findById(idPointsMarked);
         if (!pointsFounds) {
@@ -23,13 +33,13 @@ const createPointsRedeemed = async (req, res) => {
         const amountInitial = pointsFounds.amountPointsMarked;
         pointsFounds.amountPointsMarked -= amountInitial;
         await pointsFounds.save();
-
+        
         // Registrar los puntos canjeados
         const newPointsRedeemed = new PointsRedeemed({
             amountRedeemed: amountInitial,
             location: {
-                type,
-                coordinates
+                type: 'Point',
+                coordinates: [latitude, longitude]
             },
             idPointsMarked: pointsFounds.idPoints,
             receiver,
@@ -54,7 +64,38 @@ const getPointsRedeemed = async (req, res) => {
     }
 };
 
+const getPointsRedeemedById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const point = await PointsRedeemed.findById(id);
+        if (!point) {
+            return res.status(404).json({ message: 'Punto no encontrado' });
+        }
+        res.status(200).json(point);
+    } catch (error) {
+        console.error('Error al consultar el punto:', error);
+        res.status(500).json({ message: 'Error al consultar el punto', error });
+    }
+};
+
+const deletePointRedeemedById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedPoint = await PointsRedeemed.findByIdAndDelete(id);
+        if (!deletedPoint) {
+            return res.status(404).json({ message: 'Puntos no encontrados' });
+        }
+        res.status(200).json({ message: 'Puntos eliminados con éxito' });
+    } catch (error) {
+        console.error('Error al eliminar el punto:', error);
+        res.status(500).json({ message: 'Error al eliminar el punto', error });
+    }
+};
+
 module.exports = {
     createPointsRedeemed,
-    getPointsRedeemed
+    getPointsRedeemed,
+    getPointsRedeemedById,
+    deletePointRedeemedById
 };
