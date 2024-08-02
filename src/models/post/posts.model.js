1a7fead30e0fb5f421ea0d0a522e31144d223dfc
Luis Galea <luisgalea31@gmail.com>
2024-08-02 17:54:59 -0500

    fix<src/moodels/post/post.model.js>: fix discriminator

--- src/models/post/posts.model.js
+++ src/models/post/posts.model.js
@@ -3,189 +3,149 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const picSchema = new Schema({
-	public_id: {
-		type: String,
-		default: '',
+	public_id: { 
+		type: String, 
+		default: '' 
 	},
-	secure_id: {
-		type: String,
-		default: '',
+	secure_url: { 
+		type: String, 
+		default: '' 
 	},
 });
 
 const commentSchema = new Schema({
-	userId: {
-		type: String,
-		required: true,
+	userId: { 
+		type: String, 
+		required: true 
 	},
-	text: {
-		type: String,
-		default: '',
-		required: true,
+	text: { 
+		type: String, 
+		required: true 
 	},
-	date: {
-		type: String,
-		required: true,
-		default: '',
+	date: { 
+		type: String, 
+		required: true, 
+		default: '' 
 	},
-	rating: {
-		type: Number| undefined,
+	rating: { 
+		type: Number 
 	},
 });
 
 const eventSchema = new Schema({
-	name: {
-		type: String,
-		required: String,
+	name: { 
+		type: String, 
+		required: true 
 	},
-	desc: {
-		type: String,
-		required: String,
+	desc: { 
+		type: String, 
+		required: true 
 	},
-	fechaI: {
-		type: String,
-		required: String,
+	fechaI: { 
+		type: String, 
+		required: true 
 	},
-	fechaF: {
-		type: String,
-		required: String,
+	fechaF: { 
+		type: String, 
+		required: true 
 	},
-	req: {
-		type: [String],
-		default: [],
+	req: { 
+		type: [String], 
+		default: [] 
 	},
-	coordinates: {
-		type: [Number],
-		default: [],
+	coordinates: { 
+		type: Schema.Types.Mixed, 
+		required: true 
 	},
-	ubicacion: {
-		type: String,
+	ubicacion: { 
+		type: String 
 	},
-	pictures: {
-		type: [picSchema],
-		required: true,
+	pictures: { 
+		type: [picSchema], 
+		required: false 
 	},
 });
 
 const votesSchema = new Schema({
-	option1: {
-		type: [String],
-		default: [],
+	option1: { 
+		type: [String], 
+		default: [] 
 	},
-	option2: {
-		type: [String],
-		default: [],
+	option2: { 
+		type: [String], 
+		default: [] 
 	},
-	option3: {
-		type: [String],
-		default: [],
+	option3: { 
+		type: [String], 
+		default: [] 
 	},
-	option4: {
-		type: [String],
-		default: [],
+	option4: { 
+		type: [String], 
+		default: [] 
 	},
 });
 
 const pollSchema = new Schema({
-	question: {
-		type: String,
-		required: true,
-		default: '',
+	question: { 
+		type: String, 
+		required: true, 
+		default: '' 
 	},
-	desc: {
-		type: String,
-		required: true,
+	desc: { 
+		type: String, 
+		required: true 
 	},
-	options: {
-		type: [String],
-		required: true,
+	options: { 
+		type: [String], 
+		required: true 
 	},
-	votes: {
-		type: votesSchema,
+	votes: { 
+		type: votesSchema 
 	},
 });
 
 const normalSchema = new Schema({
-	title: {
-		type: String,
-		required: true,
+	title: { 
+		type: String, 
+		required: true 
 	},
-	desc: {
-		type: String,
-		required: true,
+	desc: { 
+		type: String, 
+		required: true 
 	},
-	pictures: {
-		type: [picSchema],
-		required: true,
-		default: []
+	pictures: { 
+		type: [picSchema], 
+		required: true, 
+		default: [] 
 	},
 });
 
 const postSchema = new Schema(
-	{
-		author: {
-			type: String,
-			required: true,
-		},
-		image: {
-			type: {
-				public_id: {
-					type: String,
-					default: '',
-				},
-				secure_url: {
-					type: String,
-					default: '',
-				},
-			},
-			required: true,
-		},
-		type: {
-			type: String,
-			default: 'Normal',
-			required: true,
-		},
-		content: {
-			type: Schema.Types.Mixed,
-			required: true,
-		},
-		comments : {
-			type: [commentSchema],
-			default: [],
-		},
-		likes: {
-			type: [{
-				userId: {
-					type: String,
-					required: true,
-				},
-				type: {
-					type: String,
-					enum: [], // Tipos de reacciones
-					//Like - Me gusta
-					//Me enojo
-					//Risa
-					//Recomendado
-					required: true,
-				},
-				date: {
-					type: String,
-					required: true,
-				}
-			}],
-			default: [],
-			required: true,
-		},
-		originalPost: {
-			type: String,
-			default: null,
-		  },
-	}, { timestamps: true}
+  {
+    author: { type: String, required: true },
+    image: {
+      public_id: { type: String, default: '' },
+      secure_url: { type: String, default: '' },
+    },
+    type: { type: String, required: true },
+    content: { type: Schema.Types.Mixed, required: false },
+    comments: { type: [commentSchema], default: [] },
+    likes: {
+      type: [{
+        userId: { type: String, required: true },
+        type: { type: String, enum: [], required: true },
+        date: { type: String, required: true },
+      }],
+      default: [],
+    },
+    originalPost: { type: String, default: null },
+  },
+  { timestamps: true, discriminatorKey: 'type' }
 );
 
-// discriminators
-postSchema.discriminator('Event', eventSchema);
-postSchema.discriminator('Poll', pollSchema);
-postSchema.discriminator('Normal', normalSchema);
+const Post = mongoose.model('Post', postSchema);
+const EventPost = Post.discriminator('Event', eventSchema);
+const PollPost = Post.discriminator('Poll', pollSchema);
+const NormalPost = Post.discriminator('Normal', normalSchema);
 
-module.exports = mongoose.model('posts', postSchema);
+module.exports = { Post, EventPost, PollPost, NormalPost };
