855fcce617cb980af099562521aacce45367f385
Luis Galea <luisgalea31@gmail.com>
2024-08-02 17:57:50 -0500

    feat<src/models/tokens/yokenCollection.model.js>: add ref anothers models

--- src/models/tokens/tokenCollection.model.js
+++ src/models/tokens/tokenCollection.model.js
@@ -39,14 +39,20 @@ const tokenCollectionSchema = new Schema(
       default: 0,
       required: true,
     },
-
     idCollection: {
-      type: String,
-      require: true,
+      type: Schema.Types.ObjectId, // referencia a la collection
+      ref: 'collections',
+      required: true
     },
     author: {
-      type: String,
-      require: true,
+      type: Schema.Types.ObjectId, // referencia al usuario emisor
+      ref: 'Users',
+      required: true
+    },
+    idPost: {
+      type: Schema.Types.ObjectId, // referencia al post
+      ref: 'Post',
+      required: true
     },
     buyerid: {
       type: String,
