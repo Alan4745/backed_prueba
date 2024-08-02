5c7a9dc5ff620423a77ca593c5be3a4d88596e77
Luis Galea <luisgalea31@gmail.com>
2024-08-02 17:47:02 -0500

    feat<src/controllers/token/findCollectionById.js>: modularizando funciones

---
+++ src/controllers/token/findCollectionById.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+// const crypto = require("crypto");
+// const TokenCollection = require("../../models/tokens/tokenCollection.model");
+const Collections = require("../../models/tokens/collections.model");
+// const Token = require("../../models/tokens/tokenUnitary.model");
+// const fs = require("fs-extra");
+// const User = require("../../models/user.model");
+// const io = require("../../../Server");
+
+// const { UploadImg } = require("../../utils/cloudinary");
+// const { default: mongoose } = require("mongoose");
+
+async function findCollectionById(req, res) {
+    const { collectionId } = req.params;
+    try {
+      // Buscar la colección por su nombre
+      const collection = await Collections.findById({ _id: collectionId });
+  
+      if (collection) {
+        // Si se encuentra la colección, devolverla en la respuesta
+        return res.status(200).send({ message: collection });
+      } else {
+        // Si no se encuentra la colección, devolver un mensaje de error
+        return res
+          .status(404)
+          .send({ message: "No se encontró ninguna colección con ese nombre." });
+      }
+    } catch (error) {
+      // Manejar errores
+      console.error("Error al buscar la colección:", error);
+      return res.status(500).send({ message: "Error interno del servidor." });
+    }
+}
+
+module.exports = { findCollectionById }
