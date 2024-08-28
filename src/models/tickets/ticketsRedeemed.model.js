07945290472eca9de2049c62a8e3bbcdfd6dcb81
Luis Galea <luisgalea31@gmail.com>
2024-08-28 18:56:46 -0500

    feat<src/models/tickets>: create and config new models

---
+++ src/models/tickets/ticketsRedeemed.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ticketsRedeemedSchema = new Schema({
+    amountRedeemed: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: {
+            type: String, // 'Point'
+            default: 'Point',
+            required: true
+        },
+        coordinates: {
+            type: [Number], // Array de números para coordenadas
+            required: true
+        }
+    },
+    idTicketsMarked: {
+        type: Schema.Types.ObjectId, // referencia a los puntos a canjear
+        ref: 'TicketsMarked',
+        required: true
+    },
+    receiver: {
+        type: Schema.Types.ObjectId, // referencia al usuario receptor
+        ref: 'Users',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const TicketsRedeemed = mongoose.model('TicketsRedeemed', ticketsRedeemedSchema);
+
+module.exports = { TicketsRedeemed };
