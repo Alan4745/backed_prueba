725e5975c23facfca6c48e21b6bcccdb842c636e
Developer jr Miguel Velasco <migue16velasco@gmail.com>
2024-09-13 17:56:04 -0400

    feat: add and remove  like

--- src/controllers/post/index.js
+++ src/controllers/post/index.js
@@ -1,16 +1,17 @@
 const createPost = require('./createPost');
-const { getAllPosts, 
-        getFeedPosts, 
-        getPostFollowing, 
-        getPost, 
-        getPostByUser, 
+const { getAllPosts,
+        getFeedPosts,
+        getPostFollowing,
+        getPost,
+        getPostByUser,
         getPublicPost } = require('./getPosts');
 const updatePost = require('./updatePost');
 const deletePost = require('./deletePost');
 const sharePost = require('./sharePost');
-const { addComment, 
-        deleteComment, 
+const { addComment,
+        deleteComment,
         getCommentsPost } = require('./comments');
+const { addLike, removeLike } = require('./likes');
 const reactPost = require('./react');
 
 module.exports = {
@@ -25,6 +26,8 @@ module.exports = {
   deletePost,
   sharePost,
   addComment,
+  addLike,
+  removeLike,
   getCommentsPost,
   deleteComment,
   reactPost,
