4cd025d3923682f1fb10250cc92216ab91f081b0
Eduardo-45 <alaneduardodelacruz407@gmail.com>
2024-11-13 11:21:11 -0600

    aumentamos la probalidad de los premios

--- src/controllers/token.controller.js
+++ src/controllers/token.controller.js
@@ -715,9 +715,9 @@ async function redeemTicketAdrenaline(req, res) {
     // Definir las categorías y sus probabilidades
     const categories = {
       "entrada doble": 0.5,
-      "merch adrenaline": 2.5,
-      "producto de adrenaline rush": 5,
-      participacion: 92,
+      "merch adrenaline": 5,
+      "producto de adrenaline rush": 10,
+      participacion: 84.5,
     };
 
     // Generar un número aleatorio entre 0 y 100
