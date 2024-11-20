985cd3e184898958158175939ae7fd4d2cbbea77
Luis Galea <luisgalea31@gmail.com>
2024-11-20 11:51:35 -0400

    feat<src/models/note>: add type

--- src/models/note/note.model.js
+++ src/models/note/note.model.js
@@ -1,32 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const noteSchema = new Schema({
-    senderId: {
-        type: Schema.Types.ObjectId, // referencia al usuario remitente
-        ref: 'Users',
-        required: true
-    },
-    reciverId: {
-        type: Schema.Types.ObjectId, // referencia al usuario receptor
-        ref: 'Users',
-        required: true
-    },
-    noteContent: {
-        type: String,
-        required: true
-    },
-    statusNote: {
-        type: String,
-        enum: ['public', 'anonymous'],
-        required: true
-    },
-    coordinates: {
-        type: [Number], // Array de números para coordenadas
-        required: true
-    }
+  senderId: {
+    type: Schema.Types.ObjectId, // referencia al usuario remitente
+    ref: "Users",
+    required: true,
+  },
+  reciverId: {
+    type: Schema.Types.ObjectId, // referencia al usuario receptor
+    ref: "Users",
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["normal", "ar"],
+    required: true,
+  },
+  noteContent: {
+    type: String,
+    required: true,
+  },
+  statusNote: {
+    type: String,
+    enum: ["public", "anonymous"],
+    required: true,
+  },
+  coordinates: {
+    type: [Number], // Array de números para coordenadas
+    required: true,
+  },
 });
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
 module.exports = { Note };
