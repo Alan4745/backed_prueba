d2bbc499ff23eb90bfad4b8687e9f97e2a6756a9
Luis Galea <luisgalea31@gmail.com>
2024-11-25 11:11:16 -0400

    feat<src/routes/notes>: add new routes toggleLikeNote

--- src/routes/note/note.routes.js
+++ src/routes/note/note.routes.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const md_autenticacion = require("../../middlewares/authentication.js");
-const { 
-    createNewNote, 
-    getAllNotes,  
-    getSentNotesByUser, 
-    getReceivedNotesByUser 
+const {
+  createNewNote,
+  toggleLikeNote,
+  getAllNotes,
+  getSentNotesByUser,
+  getReceivedNotesByUser,
 } = require("../../controllers/note/note.controller.js");
 
 const api = express.Router();
 
 // Rutas de notas
 api.get("/getAllNotes", [md_autenticacion.Auth], getAllNotes);
-api.get("/getSentNotesByUser/:userId", [md_autenticacion.Auth], getSentNotesByUser);
-api.get("/getReceivedNotesByUser/:userId", [md_autenticacion.Auth], getReceivedNotesByUser);
+api.get(
+  "/getSentNotesByUser/:userId",
+  [md_autenticacion.Auth],
+  getSentNotesByUser
+);
+api.get(
+  "/getReceivedNotesByUser/:userId",
+  [md_autenticacion.Auth],
+  getReceivedNotesByUser
+);
 api.post("/createNewNote", [md_autenticacion.Auth], createNewNote);
+api.post("/toggleLikeNote", [md_autenticacion.Auth], toggleLikeNote);
 
 module.exports = api;
