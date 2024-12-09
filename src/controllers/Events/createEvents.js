4cdc47954f10839f02161b1f583c548424bf4c7e
Developer jr Miguel Velasco <migue16velasco@gmail.com>
2024-12-09 09:44:39 -0400

    fixed: agregar imagen

--- src/controllers/Events/createEvents.js
+++ src/controllers/Events/createEvents.js
@@ -1,41 +1,30 @@
-const fs = require('fs');
 const { Events } = require('../../models/events/events.model');
 const { UploadImg } = require("../../utils/cloudinary");
-
+const fs = require("fs-extra");
 
 const createEvent = async (req, res) => {
+    // console.log(req.files.image)
     try {
         let image = {};
-        let imagens = [];
 
         // Cargar imagen principal
         if (req.files?.image) {
-            try {
-                const result = await UploadImg(req.files.image.tempFilePath);
-                image.public_id = result.public_id;
-                image.secure_url = result.secure_url;
-
-                if (fs.existsSync(req.files.image.tempFilePath)) {
-                    await fs.unlink(req.files.image.tempFilePath);
-                } else {
-                    console.warn("El archivo temporal de la imagen principal no existe.");
-                }
-            } catch (uploadError) {
-                console.error("Error al subir imagen principal:", uploadError);
-                return res.status(500).json({
-                    status: 'error',
-                    message: 'Error al subir la imagen principal'
-                });
+            const result = await UploadImg(req.files.image.tempFilePath);
+            image.public_id = result.public_id;
+            image.secure_url = result.secure_url;
+            if (fs.existsSync(req.files.image.tempFilePath)) {
+                await fs.unlink(req.files.image.tempFilePath);
+            } else {
+                console.warn("El archivo temporal no existe.");
             }
         }
-
-
 
         // Preparar datos del evento
         const eventData = {
             title: req.body.title,
             type: req.body.type,
             coordinates: JSON.parse(req.body.coordinates),
+            radio: req.body.radio, // Añadido el campo 'radio'
             dateInit: req.body.dateInit,
             dateEnd: req.body.dateEnd,
             image: image,
@@ -59,6 +48,7 @@ const createEvent = async (req, res) => {
         });
     }
 };
+
 
 
 module.exports = {
