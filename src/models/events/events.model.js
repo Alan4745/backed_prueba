3b0d9d5a5e3ecb9589ede294fdbb4f7d3d6f9b5d
Developer jr Miguel Velasco <migue16velasco@gmail.com>
2024-12-09 09:19:02 -0400

    feat: create model event

---
+++ src/models/events/events.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const eventsSchema = new Schema({
+    image: {
+        public_id: { type: String, default: "" },
+        secure_url: { type: String, default: "" },
+    },
+    title: {
+        type: String,
+        require: true,
+    },
+    type: {
+        type: String,
+        enum: ["private", "public"],
+        required: true,
+    },
+    coordinates: {
+        type: [Number], // Array de números para coordenadas
+        required: true,
+    },
+    dateInit: {
+        type: String,
+        required: true,
+        default: "",
+    },
+    dateEnd: {
+        type: String,
+        required: true,
+        default: "",
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Events = mongoose.model('Events', eventsSchema);
+
+module.exports = { Events };
