ac363dc8623a364320845ed24f21e8a71fad5482
Developer jr Miguel Velasco <migue16velasco@gmail.com>
2024-12-13 09:08:09 -0400

    feat: add event router

--- src/routes/Events/events.router.js
+++ src/routes/Events/events.router.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const md_autenticacion = require('../../middlewares/authentication');
 const {
-    createEvent
+    createEvent,
 } = require("../../controllers/Events/createEvents");
+const {
+    getEventsByUser,
+} = require("../../controllers/Events/getEventsByUser");
+
+const {
+    getReceivedEventsByUser,
+} = require("../../controllers/Events/getReceivedEventsByUser");
 
 const api = express.Router();
 api.post("/createEvent", createEvent);
+api.get(
+    "/getSentEventsByUser/:userId",
+    getEventsByUser
+);
+api.get(
+    "/getReceivedEventByUser/:userId",
+    getReceivedEventsByUser
+);
 
 module.exports = api;
