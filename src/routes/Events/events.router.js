ec2cd8257d3c1ad8d0fc8204057076abe0dffc70
Luis Galea <luisgalea31@gmail.com>
2024-12-16 09:34:35 -0400

    feat<src/routes/events>: create new routes of events

--- src/routes/Events/events.router.js
+++ src/routes/Events/events.router.js
@@ -1,26 +1,19 @@
-
-const express = require('express');
-const md_autenticacion = require('../../middlewares/authentication');
-const {
-    createEvent,
-} = require("../../controllers/Events/createEvents");
-const {
-    getEventsByUser,
-} = require("../../controllers/Events/getEventsByUser");
+const express = require("express");
+const md_autenticacion = require("../../middlewares/authentication");
+const { createEvent } = require("../../controllers/Events/createEvents");
+const { getEventsByUser } = require("../../controllers/Events/getEventsByUser");
 
 const {
-    getReceivedEventsByUser,
+  getReceivedEventsByUser,
 } = require("../../controllers/Events/getReceivedEventsByUser");
+const {
+  eventsDetectByKmRadius,
+} = require("../../controllers/Events/eventsDetectByKmRadius.controller");
 
 const api = express.Router();
 api.post("/createEvent", createEvent);
-api.get(
-    "/getSentEventsByUser/:userId",
-    getEventsByUser
-);
-api.get(
-    "/getReceivedEventByUser/:userId",
-    getReceivedEventsByUser
-);
+api.post("/getPerimeterEventsByCurrentUbication", eventsDetectByKmRadius);
+api.get("/getSentEventsByUser/:userId", getEventsByUser);
+api.get("/getReceivedEventByUser/:userId", getReceivedEventsByUser);
 
 module.exports = api;
