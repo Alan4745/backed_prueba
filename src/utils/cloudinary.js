46aaf5be4e6b1452107cefc0e481ccb37175fb96
Luis Galea <luisgalea31@gmail.com>
2024-12-16 10:14:38 -0400

    feat<src/utils>: create new func upload audio

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,28 +1,35 @@
-const cloudinary = require('cloudinary').v2;
-const { CLOUD_NAME, API_KEY, API_SECRET_KEY } = require('../../configs/config');
+const cloudinary = require("cloudinary").v2;
+const { CLOUD_NAME, API_KEY, API_SECRET_KEY } = require("../../configs/config");
 
 cloudinary.config({
-	cloud_name: CLOUD_NAME,
-	api_key: API_KEY,
-	api_secret: API_SECRET_KEY,
-	secure: true
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET_KEY,
+  secure: true,
 });
 
 async function UploadImg(filePath) {
-	return await cloudinary.uploader.upload(filePath, {
-		folder: 'replit',
-	});
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "replit",
+  });
 }
 
 async function UploadVideo(filePath) {
-	return await cloudinary.uploader.upload(filePath, {
-		folder: 'video',
-		resource_type: 'video'
-	});
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "video",
+    resource_type: "video",
+  });
 }
 
+async function UploadAudio(filePath) {
+  return await cloudinary.uploader.upload(filePath, {
+    folder: "audio",
+    resource_type: "video", // Cloudinary usa "video" para manejar archivos de audio también
+  });
+}
 
 module.exports = {
-	UploadImg,
-	UploadVideo
+  UploadImg,
+  UploadVideo,
+  UploadAudio,
 };
