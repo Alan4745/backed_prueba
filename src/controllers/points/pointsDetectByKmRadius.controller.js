d0879090f774a4f1b340700389e5106cfcbfccb9
Luis Galea <luisgalea31@gmail.com>
2024-12-16 10:13:41 -0400

    fix<src/controllers>: fix radius 15,000 meters

--- src/controllers/points/pointsDetectByKmRadius.controller.js
+++ src/controllers/points/pointsDetectByKmRadius.controller.js
@@ -1,23 +1,29 @@
-const { PointsMarked } = require('../../models/points/pointsMarked.model');
-const { verifyRedemption } = require('../../funcs/verifyRedemption');
+const { PointsMarked } = require("../../models/points/pointsMarked.model");
+const { verifyRedemption } = require("../../funcs/verifyRedemption");
 
 const pointsDetectByKmRadius = async (req, res) => {
   try {
     const { coordinates } = req.body; // Las coordenadas llegan por el req.body como [latitud, longitud]
     if (!coordinates || coordinates.length !== 2) {
-      return res.status(400).json({ success: false, message: 'Coordenadas no válidas' });
+      return res
+        .status(400)
+        .json({ success: false, message: "Coordenadas no válidas" });
     }
 
     // Obtener todos los puntos sin aplicar filtro de distancia
     const allPoints = await PointsMarked.find();
 
-    // Verificar cada punto para determinar si está dentro del radio de 500 km (500,000 metros)
-    const radiusInMeters = 500 * 1000;
+    // Verificar cada punto para determinar si está dentro del radio de 15 km (15,000 metros)
+    const radiusInMeters = 15 * 1000;
 
     // Filtrar los puntos que están dentro del radio usando la función verifyRedemption
     const pointsFound = await Promise.all(
       allPoints.map(async (point) => {
-        const verificationResult = await verifyRedemption(point, coordinates, radiusInMeters);
+        const verificationResult = await verifyRedemption(
+          point,
+          coordinates,
+          radiusInMeters
+        );
         // Retornar solo los puntos que están dentro del radio
         if (verificationResult.success) {
           return point;
@@ -32,14 +38,17 @@ const pointsDetectByKmRadius = async (req, res) => {
     if (!filteredPoints.length) {
       return res.status(404).json({
         success: false,
-        message: 'No se encontraron puntos dentro del radio especificado',
+        message: "No se encontraron puntos dentro del radio especificado",
       });
     }
 
     return res.status(200).json({ success: true, pointsFound: filteredPoints });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Error al buscar puntos en el radio especificado' });
+    res.status(500).json({
+      success: false,
+      message: "Error al buscar puntos en el radio especificado",
+    });
   }
 };
 
--- src/controllers/tickets/ticketsDetectByKmRadius.controller.js
+++ src/controllers/tickets/ticketsDetectByKmRadius.controller.js
@@ -1,23 +1,29 @@
-const { TicketsMarked } = require('../../models/tickets/ticketsMarked.model');
-const { verifyRedemption } = require('../../funcs/verifyRedemption');
+const { TicketsMarked } = require("../../models/tickets/ticketsMarked.model");
+const { verifyRedemption } = require("../../funcs/verifyRedemption");
 
 const ticketsDetectByKmRadius = async (req, res) => {
   try {
     const { coordinates } = req.body; // Las coordenadas llegan por el req.body como [latitud, longitud]
     if (!coordinates || coordinates.length !== 2) {
-      return res.status(400).json({ success: false, message: 'Coordenadas no válidas' });
+      return res
+        .status(400)
+        .json({ success: false, message: "Coordenadas no válidas" });
     }
 
     // Obtener todos los tickets sin aplicar filtro de distancia
     const allTickets = await TicketsMarked.find();
 
-    // Verificar cada tickets para determinar si está dentro del radio de 500 km (500,000 metros)
-    const radiusInMeters = 500 * 1000;
+    // Verificar cada tickets para determinar si está dentro del radio de 15 km (15,000 metros)
+    const radiusInMeters = 15 * 1000;
 
     // Filtrar los tickets que están dentro del radio usando la función verifyRedemption
     const ticketsFound = await Promise.all(
       allTickets.map(async (point) => {
-        const verificationResult = await verifyRedemption(point, coordinates, radiusInMeters);
+        const verificationResult = await verifyRedemption(
+          point,
+          coordinates,
+          radiusInMeters
+        );
         // Retornar solo los tickets que están dentro del radio
         if (verificationResult.success) {
           return point;
@@ -32,14 +38,19 @@ const ticketsDetectByKmRadius = async (req, res) => {
     if (!filteredTickets.length) {
       return res.status(404).json({
         success: false,
-        message: 'No se encontraron tickets dentro del radio especificado',
+        message: "No se encontraron tickets dentro del radio especificado",
       });
     }
 
-    return res.status(200).json({ success: true, ticketsFound: filteredTickets });
+    return res
+      .status(200)
+      .json({ success: true, ticketsFound: filteredTickets });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ success: false, message: 'Error al buscar tickets en el radio especificado' });
+    res.status(500).json({
+      success: false,
+      message: "Error al buscar tickets en el radio especificado",
+    });
   }
 };
 
